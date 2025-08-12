8335fb19bdfad7d6532f3a3eeb4d0cd85fa9d86a
ParthibBiswas10 <parthiblm10@gmail.com>
2025-08-12 23:40:13 +0530

    Q2

---
+++ ASSIGNMENT_5/q2.java
@@ -0,0 +1,23 @@
+/*5.2 Write a program by using class with the following specifications:
+Class name — Sale
+Data members/ Instance variables:
+1. String title, author, publication
+2. double price
+Member methods:
+1. void input() — to accept title, author name and publication name and price of a book
+2. void display() — to display title, author name and publication name and price of a book
+Now, create another class 'Purchase' that inherits class 'Sale' having the following specifications:
+Class name — Purchase
+Data members/ Instance variables:
+1. int noc
+2. int amount;
+Member methods:
+1. void accept() — to enter the number of copies purchased
+2. void calculate( ) — to find the amount by multiplying number of copies ordered and price (i.e.,
+noc * price)
+3. void show() — to display the elements describes in base class along with the number of copies
+purchased and amount to be paid to the shopkeeper */
+
+
+
+
